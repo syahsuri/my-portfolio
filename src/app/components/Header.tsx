5daa33b5d85ebf5aa40163e15c7464c6b2d8f3be
syahsury <officialsyahsury@gmail.com>
2025-06-26 22:12:02 +0700

    cv add

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -32,7 +32,7 @@ const Header = () => {
           <Image src={assets.right_arrow_white} alt="" className="w-4" />
         </a>
         <a
-          href="/sample-resume.pdf"
+          href="/Syah-Sury-Alam-Software-Engineer.pdf"
           download
           className="px-10 py-3 border rounded-2xl border-gray-500 flex items-center gap-2">
           My Resume <Image src={assets.download_icon} alt="" className="w-4" />
